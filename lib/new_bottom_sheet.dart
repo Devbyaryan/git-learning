3ba2c431ed430fa245b7ae74795ef58105544604
Devbyaryan <aryantonk77@gmail.com>
2025-04-11 15:45:01 +0530

    new bottom sheet added

---
+++ lib/new_bottom_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewBottomSheet extends StatelessWidget {
+  const NewBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Hello');
+  }
+}
